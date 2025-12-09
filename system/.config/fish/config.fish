63ce8e840685f3c62039754e4f39a29804ac0def
akai-hana <akaihanatsundeee@proton.me>
2025-12-09 17:04:19 +0100

    major sync

---
+++ system/.config/fish/config.fish
@@ -0,0 +1,20 @@
+abbr -a fishconf "vim ~/.config/fish/config.fish"
+abbr -a xinitrc "vim ~/.xinitrc"
+abbr -a xprofile "vim ~/.xprofile"
+
+abbr -a picom "picom --backend glx"
+
+abbr -a suckstall "doas bmake clean install"
+abbr -a ff "fastfetch"
+abbr -a sx "startx"
+
+abbr -a get "doas apk add"
+abbr -a seget "apk search"
+abbr -a unget "doas apk del"
+abbr -a upd "doas apk update; doas apk upgrade"
+abbr -a zawarudo "doas apk update; doas apk upgrade"
+
+abbr -a search "ls | grep -i"
+
+# Created by `pipx` on 2025-12-08 15:02:37
+set PATH $PATH /home/akai/.local/bin
